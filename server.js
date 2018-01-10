b44ae856583b604c66774ba269c6c728e5ab6de1
Franziska Hinkelmann <franzih@chromium.org>
2018-01-10 11:32:32 +0100

    PoC that tics are not useful, we need to look at the stack for calling functions

--- server.js
+++ server.js
@@ -12,48 +12,26 @@ function outer() {
     const upper_bound = 10000;
     let sum = 0;
 
-    for(let i = 0; i < upper_bound; i++) {
-        sum += inner({x: 1});
-        sum += inner({y: 12, x: 1});
-        sum += inner({x: 1.0});
-        sum += inner({y: 2, z: 4, x: 1});
-        sum += inner({a: 2, z: 4, x: 1});
-        sum += inner({b: 2, z: 4, x: 1});
+    sum += inner({x: 1});
 
-        let objects = [
-            {x: 1},
-            {y: 12, x: 1},
-            {x: 1.0},
-            {y: 2, z: 4, x: 1},
-            {a: 2, z: 4, x: 1},
-            {b: 2, z: 4, x: 1}
-        ];
 
-        for (let obj of objects) {
-            sum += obj.x;
-        }
+    let objects = [
+        {x: 1},
+        {y: 12, x: 1},
+        {x: 1.0},
+        {y: 2, z: 4, x: 1},
+        {a: 2, z: 4, x: 1},
+        {b: 2, z: 4, x: 1}
+    ];
 
-        let obj = {x: 1};
-        sum += obj.x;
-
-        obj =  {y: 12, x: 1};
-        sum += obj.x;
-
-        obj = {x: 1.0};
-        sum += obj.x;
-
-        obj = {y: 2, z: 4, x: 1};
-        sum += obj.x;
-
-        obj = {a: 2, z: 4, x: 1};
-        sum += obj.x;
-
-        obj = {b: 2, z: 4, x: 1};
+    for (let obj of objects) {
         sum += obj.x;
     }
 
-    setImmediate(outer);
+    sum += inner({b: 2, z: 4, x: 1}, upper_bound);
 
+
+    setImmediate(outer);
 }
 
 
