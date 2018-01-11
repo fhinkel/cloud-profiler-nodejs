867c4c8b860c4dac15a1b0183b93fd1e767ea0cb
Franziska Hinkelmann <franzih@chromium.org>
2018-01-11 15:03:16 +0100

    Make outer call inner in two if statements

--- server.js
+++ server.js
@@ -9,29 +9,27 @@ function inner(obj, count = 1){
 }
 
 function outer() {
+    let sum = 0;
     const upper_bound = 10000;
-    let total = 0;
 
-    let objects = [
-        {x: 1},
-        {y: 12, x: 1},
-        {x: 1.0},
-        {y: 2, z: 4, x: 1},
-        {a: 2, z: 4, x: 1},
-        {b: 2, z: 4, x: 1}
-    ];
+    if (false) {
+        sum += inner({b: 2, z: 4, x: 1}, upper_bound);
+    }
 
-    let obj = objects[0];
-
+    let obj = {x: 1};
     let res = 0;
     for(let i = 0; i < upper_bound; i++) {
         res += obj.x;
     }
 
-    let sum = inner({b: 2, z: 4, x: 1}, upper_bound);
+    if (true) {
+        sum += inner({b: 2, z: 4, x: 1}, upper_bound);
+    }
 
-    total += sum + res;
-
+    let total = sum + res;
+    if (total === 17) {
+        console.log(total);
+    }
     setImmediate(outer);
 }
 
